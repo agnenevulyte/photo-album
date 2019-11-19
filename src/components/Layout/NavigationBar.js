ddde18f26301632dbe9a1640d6e15f0315409d87
agnenevulyte <you@example.com>
2019-11-19 19:13:00 +0000

    navigation bar unstyled

--- src/components/Layout/NavigationBar.js
+++ src/components/Layout/NavigationBar.js
@@ -5,9 +5,9 @@ export default function NavigationBar() {
     return (
         <Navbar bg="dark" variant="dark">
             <Nav className="mr-auto">
-                <Nav.Link><Link to="/">Home</Link></Nav.Link>
-                <Nav.Link><Link to="/about">About</Link></Nav.Link>
-                <Nav.Link><Link to="/docs">Documentation</Link></Nav.Link>
+               <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/docs">Documentation</Link>
             </Nav>
                 <a href="https://github.com/agnenevulyte">
                     <Image src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" roundedCircle className="githubIcon" />
