e74a1515a8f953980cafd08242cdcf7596c45747
agnenevulyte <you@example.com>
2019-11-19 12:57:41 +0000

    started routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 
 import './App.css';
 import NavigationBar from './components/Layout/NavigationBar';
 import Home from './components/Home/index'
+import About from  './components/About'
+import Docs from './components/Docs'
+import NotFound from './NotFound'
 import Footer from './components/Layout/Footer';
 
 function App() {
   return (
     <div className="App">
-        <NavigationBar />
-        <Home />
+      <NavigationBar />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='/docs' component={Docs}/>
+          <Route component={NotFound} />
+        </Switch>
         <Footer/>
     </div>
   );
