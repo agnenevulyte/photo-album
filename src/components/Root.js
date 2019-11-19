b3290090c9443c30c23415db15454e4bacbdf4b2
agnenevulyte <you@example.com>
2019-11-19 12:39:08 +0000

    created Root.js

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import App from "./App"
+
+export default function Root() {
+    return (
+        <Router>
+            <Route path="/" component={App} />
+        </Router>
+    )
+}
