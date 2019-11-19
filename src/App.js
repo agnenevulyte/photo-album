7fa8b8c1d67d5c99464f038159b4afead120922d
agnenevulyte <you@example.com>
2019-11-19 15:41:53 +0000

    routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React from "react";
+// import { Route, Switch } from "react-router-dom";
 
 import './App.css';
-import NavigationBar from './components/Layout/NavigationBar';
-import Home from './components/Home/index'
-import About from  './components/About'
-import Docs from './components/Docs'
+// import Home from './components/Home/index'
+// import About from  './components/About'
+// import Docs from './components/Docs'
 // import NotFound from './NotFound'
-import Root from './components/Root'
 import Footer from './components/Layout/Footer';
+import Root from './components/Root'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <NavigationBar /> */}
-        {/* <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About}/>
-          <Route path='/docs' component={Docs}/>
-          <Route component={NotFound} />
-        </Switch> */}
-        <Root />
-        <Footer/>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <Root />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Route , Switch} from "react-router-dom"
-// import App from "./App"
-import Home from './Home'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from "react-router-dom";
+// import App from "./App";
+import NavigationBar from './Layout/NavigationBar'
+import Home from './Home/index'
 import About from  './About'
 import Docs from './Docs'
 import NotFound from '../NotFound'
-import NavigationBar from './Layout/NavigationBar';
-export default function Root() {
-    return (
-        <Router>
-            {/* <Route path="/" component={App} /> */}
-            <NavigationBar />
-            <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route path='/about' component={About}/>
-                <Route path='/docs' component={Docs}/>
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
-    )
-}
+
+const Root = () => (
+  <Router>
+    <NavigationBar />
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/docs' component={Docs}/>
+      <Route component={NotFound} />
+    </Switch>
+    {/* <Route path="/" component={App} /> */}
+  </Router>
+);
+
+export default Root;
