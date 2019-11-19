797eb3afd604e2835b1402fbf2692822ac1612f6
agnenevulyte <you@example.com>
2019-11-19 10:31:32 +0000

    navbar and footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import NavigationBar from './components/Layout/NavigationBar';
+import Footer from './components/Layout/Footer';
 
 function App() {
   return (
     <div className="App">
-
-
+        <NavigationBar />
+        <Footer/>
     </div>
   );
 }
