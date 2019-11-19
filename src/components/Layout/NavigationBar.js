a5a863a49d639a03e19470bb5ad99586634e187f
agnenevulyte <you@example.com>
2019-11-19 10:25:41 +0000

    Navigarion

---
+++ src/components/Layout/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navbar, Nav, Form, Image } from 'react-bootstrap';
+export default function NavigationBar() {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#documentation">Documentation</Nav.Link>
+            </Nav>
+                <a href="https://github.com/Joeyryanbridges">
+                    <Image src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" roundedCircle className="githubIcon" />
+                </a>
+     
+        </Navbar>
+    )
+}
