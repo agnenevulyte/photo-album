9e44e376980a767f3a97a842ee759eba0ad3d8ed
agnenevulyte <you@example.com>
2019-12-05 20:59:56 +0000

    added actions for Photo/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,26 @@
 // we have a property like itemId, when we want to update the state
 import { setActionTypes } from "./constants";
 
+// for Photos/index.js
 export const photosLoaded = photos => ({
     type: setActionTypes.PHOTOS_LOADED,
     photos
 });
-  
+ 
+// for Posts/index.js
 export const postsLoaded = posts => ({
   type: setActionTypes.POSTS_LOADED,
   posts
+});
+
+// for Photo/index.js
+export const pictureLoaded = picture => ({
+  type: setActionTypes.PICTURE_LOADED,
+  picture
+});
+export const pictureLoading = () => ({
+  type: setActionTypes.PICTURE_LOADING,
+});
+export const pictureLoadingError = () => ({
+  type: setActionTypes.PICTURE_LOADING_ERROR,
 });
