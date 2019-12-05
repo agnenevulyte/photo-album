947d4ea82c4418cdb7468e5143b615e958bf8e53
agnenevulyte <you@example.com>
2019-12-05 20:59:41 +0000

    added constants

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,5 +1,8 @@
 // Redux also suggest defining constants for your action types.
 export const setActionTypes = {
     PHOTOS_LOADED: "PHOTOS_LOADED",
-    POSTS_LOADED: "POSTS_LOADED"
+    POSTS_LOADED: "POSTS_LOADED",
+    PICTURE_LOADED: "PICTURE_LOADED",
+    PICTURE_LOADING: "PICTURE_LOADING",
+    PICTURE_LOADING_ERROR: "PICTURE_LOADING_ERROR"
   };
