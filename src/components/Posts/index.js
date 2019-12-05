c1e3bd45892968fbfbd58eecb412ad5ed033cd8a
agnenevulyte <you@example.com>
2019-12-05 19:43:11 +0000

    added redux to Posts component

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { postsLoaded } from '../../actions'
 
-export default class Posts extends Component {
+class Posts extends Component {
     constructor() {
         super();
-        this.state = {
-            posts: []
-        }
+        // this.state = {
+        //     posts: []
+        // }
     }
 
     componentDidMount() {
@@ -15,16 +18,22 @@ export default class Posts extends Component {
     fetchPosts = () => {
         fetch('http://jsonplaceholder.typicode.com/posts')
         .then(res => res.json())
-        .then(posts => this.setState({posts: posts}))
+        .then(posts => this.props.postsLoaded(posts))
     }
 
 
     render() {
         return (
             <div>
+                <Form>  
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                    {/* <Form.Label>Add a new Post</Form.Label> */}
+                    <Form.Control as="textarea" placeholder="Add a new Post" rows="3" />
+                    </Form.Group>
+                </Form>
                 <h1 className="postsPageTitle">Today's Posts!</h1>
                 <ul>
-                    {this.state.posts.map(post => 
+                    {this.props.posts.map(post => 
                     <li key={post.id}>
                         <p className="postTitle">{post.title}</p>
                         <p className="postBody">{post.body}</p></li>
@@ -34,3 +43,13 @@ export default class Posts extends Component {
         )
     }
 }
+
+const mapStateToProps = ({posts}) => ({
+     posts
+})
+
+const mapDispatchToProps = {
+    postsLoaded
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Posts)
