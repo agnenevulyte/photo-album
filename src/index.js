7dd99036a28563a0c7472e2c66588581c480809a
agnenevulyte <you@example.com>
2019-12-05 19:14:55 +0000

    added store, Provider to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import combineReducers from "./reducers";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Root from './components/Root'
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const store = createStore(
+    combineReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('root'));
 
