0f83c33c012f37a7e5a6ebe2b9551890df8c3022
agnenevulyte <you@example.com>
2019-12-05 21:00:50 +0000

    combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { photosReducer } from "./photosReducer";
 import { postsReducer } from "./postsReducer";
+import { photoReducer } from "./photoReducer"
 
 //import and add more child reducers as your project builds.
 // then I use that child in my component in mapStateToProps
 export default combineReducers({
   photos: photosReducer,
-  posts: postsReducer
+  posts: postsReducer,
+  photo: photoReducer
 });
