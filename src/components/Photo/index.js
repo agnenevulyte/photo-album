6f3137defb23f1b0398b68a66fe89a183558b9a1
Agne Nevulyte <agne.nevulyte@gmail.com>
2021-04-07 11:00:08 +0300

    changed Photo class into function

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -1,62 +1,57 @@
-import React, { Component } from 'react'
-import { fetchPicture } from './fetchPicture'
-import { connect } from 'react-redux'
-import { pictureLoaded, pictureLoading, pictureLoadingError } from '../../actions' 
+import React, { useEffect } from "react";
+import { fetchPicture } from "./fetchPicture";
+import { connect } from "react-redux";
+import {
+  pictureLoaded,
+  pictureLoading,
+  pictureLoadingError,
+} from "../../actions";
+import { useRouteMatch } from "react-router-dom";
 
-class Photo extends Component {
-    constructor(props) {
-        super(props);
-        // this.state = {
-        //     // https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
-        //     picture: {},
-        //     loading: true,
-        //     error: null 
-        // }
-    }
+const Photo = ({
+  pictureLoaded,
+  pictureLoading,
+  pictureLoadingError,
+  picture,
+  error,
+  loading,
+}) => {
+  const match = useRouteMatch();
+  useEffect(() => {
+    pictureLoading();
+    fetchPicture(match.params.id)
+      .then((results) => {
+        console.log("results:", results);
+        pictureLoaded(results);
+      })
+      .catch((error) => {
+        console.log(error);
+        pictureLoadingError(error);
+      });
+  }, []);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  if (!picture.id) return "No photo!";
 
-    componentDidMount() {
-        this.props.pictureLoading();
-        fetchPicture(this.props.match.params.id)
-        .then(results => {
-            console.log('results:', results);
-            this.props.pictureLoaded( 
-                results
-            )
-        })
-        .catch(error => {
-            console.log(error)
-            this.props.pictureLoadingError(
-                error
-            )
-        })
-    }
-  
-    render() {
-        const { picture, loading, error } = this.props;
-        if (loading) return 'Loading...'
-        if (error) return  `Error! ${error.message}`
-        if (!picture.id) return 'No photo!'
-        return (
-            <div>
-                <img src={picture.image} alt={picture.id}/>
-                <h4>{picture.name}</h4>
-                <p>{picture.status}</p>
-            </div>
-        )
-    }
-}
+  return (
+    <div>
+      <img src={picture.image} alt={picture.id} />
+      <h4>{picture.name}</h4>
+      <p>{picture.status}</p>
+    </div>
+  );
+};
 
+const mapStateToProps = ({ photo: { picture, loading, error } }) => ({
+  picture,
+  loading,
+  error,
+});
 
-const mapStateToProps = ({photo:{picture, loading, error}}) => ({
-    picture,
-    loading,
-    error
-})
-const mapDispatchToProps ={
-    pictureLoaded,
-    pictureLoading,
-    pictureLoadingError
+const mapDispatchToProps = {
+  pictureLoaded,
+  pictureLoading,
+  pictureLoadingError,
+};
 
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Photo)
+export default connect(mapStateToProps, mapDispatchToProps)(Photo);
